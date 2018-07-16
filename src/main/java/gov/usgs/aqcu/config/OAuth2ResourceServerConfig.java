28dc7cdf3c1ceca0bb848835e0dff897db4e981e
zmoore-usgs <zmoore@usgs.gov>
2018-07-16 10:43:10 -0500

    Import cleanup.

--- src/main/java/gov/usgs/aqcu/config/OAuth2ResourceServerConfig.java
+++ src/main/java/gov/usgs/aqcu/config/OAuth2ResourceServerConfig.java
@@ -1,7 +1,6 @@
 package gov.usgs.aqcu.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.security.oauth2.client.feign.OAuth2FeignRequestInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
@@ -9,8 +8,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.client.OAuth2ClientContext;
-import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
